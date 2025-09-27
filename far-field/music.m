01da917710e861918e839d5e77351c3f5e332fe2
Paulo R. A. Candido Jr. <paulocandido815@gmail.com>
2025-09-27 11:20:37 -0300

    Update music.m

--- far-field/music.m
+++ far-field/music.m
@@ -1,36 +1,40 @@
-function [musicpseudospectrum, eigenvalues] = music(Y, M, theta, snapshots, delta, lambda, method)
+function [musicpseudospectrum, eigenval] = music(Y, M, ...
+    theta, snapshots, delta, lambda, method)
     
     R = (Y * Y') / snapshots; % matriz de covariancia media
 
     % decomposição em autovalores e autovetores
-    [eigenvectors, eigenvalues] = eig(R);
-    eigenvalues_sorted = sort(diag(eigenvalues), 'descend'); % ordenar em ordem decrescente
-    eigenvalues_sorted = eigenvalues_sorted / sum(eigenvalues_sorted); % normalização dos autovalores
+    [eigenvec, eigenval] = eig(R);
+    eigenval_sorted = sort(diag(eigenval), 'descend'); 
+    eigenval_sorted = eigenval_sorted / sum(eigenval_sorted); 
     
     % escolher o metodo de estimação do numero de fontes
     switch lower(method)
         case 'aic'
-            aic = aic_estimation(M, eigenvalues_sorted, snapshots);
+            aic = aic_estimation(M, eigenval_sorted, snapshots);
             [~, estimated_sources] = min(aic);
-            disp(['Número estimado de fontes (AIC) para M = ', num2str(M), ': ', num2str(estimated_sources)]);
+            disp(['Número estimado de fontes (AIC) para M = ', ...
+                num2str(M), ': ', num2str(estimated_sources)]);
         
         case 'gap'
-            estimated_sources = gap_estimation(eigenvalues_sorted);
-            disp(['Número estimado de fontes (GAP) para M = ', num2str(M), ': ', num2str(estimated_sources)]);
+            estimated_sources = gap_estimation(eigenval_sorted);
+            disp(['Número estimado de fontes (GAP) para M = ', ...
+                num2str(M), ': ', num2str(estimated_sources)]);
         
-        case 'mdl'
-            mdl = mdl_estimation(M, eigenvalues_sorted, snapshots);
-            [~, estimated_sources] = min(mdl);
-            disp(['Número estimado de fontes (MDL) para M = ', num2str(M), ': ', num2str(estimated_sources)]);
+        % case 'mdl'
+        %     mdl = mdl_estimation(M, eigenval_sorted, snapshots);
+        %     [~, estimated_sources] = min(mdl);
+        %     disp(['Número estimado de fontes (MDL) para M = ', ...
+        %         num2str(M), ': ', num2str(estimated_sources)]);
 
         otherwise
-            error('Método inválido. Escolha entre "aic", "gap" ou "mdl".');
+            error('error. Choice "aic" or "gap"');
     end
 
     % selecionar autovetores do espaço de ruido
-    [~, i] = sort(diag(eigenvalues), 'descend');
-    eigenvectors = eigenvectors(:, i);
-    Vn = eigenvectors(:, estimated_sources+1:end);
+    [~, i] = sort(diag(eigenval), 'descend');
+    eigenvec = eigenvec(:, i);
+    Vn = eigenvec(:, estimated_sources+1:end);
 
     % inicializar o pseudoespectro MUSIC
     Pmusic = zeros(size(theta));
@@ -40,7 +44,7 @@ function [musicpseudospectrum, eigenvalues] = music(Y, M, theta, snapshots, delt
         Pmusic(i) = 1 / (a' * (Vn * Vn') * a);
     end
 
-    Pmusic = abs(Pmusic) / max(abs(Pmusic)); % normalizacao do pseudoespectro
+    Pmusic = abs(Pmusic) / max(abs(Pmusic)); 
     musicpseudospectrum = Pmusic;
-    eigenvalues = eigenvalues_sorted;
+    eigenval = eigenval_sorted;
 end
